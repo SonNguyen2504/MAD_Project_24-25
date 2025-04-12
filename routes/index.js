eedaf92c69f16886ce1b783f6614248693c3c5ca
Son Nguyen <trgson250411@gmail.com>
2025-04-12 17:39:20 +0700

    update index file in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const AuthRoute = require('./AuthRoute');
 const FoodRoute = require('./FoodRoute');
+const UserRoute = require('./UserRoute');
 
 const route = (app) => {
     app.use('/api/auth', AuthRoute);
+    app.use('/api/user', UserRoute);
     app.use('/api/food', FoodRoute);
 }
 
