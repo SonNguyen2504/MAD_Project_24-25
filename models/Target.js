7e45126fee1e08b4172efdf88ca149eda742df2c
Son Nguyen <trgson250411@gmail.com>
2025-04-29 23:28:12 +0700

    update target model and route

--- models/Target.js
+++ models/Target.js
@@ -10,10 +10,6 @@ const targetSchema = new Schema({
         type: String,
         required: true,
     },
-    isCompleted: {
-        type: Boolean,
-        default: false,
-    },
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User',
